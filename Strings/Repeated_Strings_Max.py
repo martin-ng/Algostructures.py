a6d42c0368f994eba7f292ef3f763bc8e96ee061
Martin Ng <martinng1992@gmail.com>
2020-05-04 13:22:18 -0400

    algorithm to return max repeated characters

---
+++ Strings/Repeated_Strings_Max.py
@@ -0,0 +1,27 @@
+
+def max_repeated_chars(string, max):
+    if len(string) == 0 or max == 0:
+        return
+
+    new_str = ''
+    n = len(string)
+    i = 0
+    j = 0
+    curr = string[i]
+
+    while i < n:
+        if string[i] == curr:
+            if j < max:
+                new_str += curr
+                j += 1
+            i += 1
+        else:
+            curr = string[i]
+            j = 0
+
+    return new_str
+
+
+print(max_repeated_chars('aaaabbbbccccdd', 1))
+print(max_repeated_chars('aaabbbcccccddddd', 3))
+print(max_repeated_chars('mmmaaaaarrtttin', 3))
