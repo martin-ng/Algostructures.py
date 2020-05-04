6af567df02f756f5f6e9e8a198357561160e7434
Martin Ng <martinng1992@gmail.com>
2020-05-04 13:34:24 -0400

    second solution

--- Strings/Repeated_Strings_Max.py
+++ Strings/Repeated_Strings_Max.py
@@ -21,7 +21,29 @@ def max_repeated_chars(string, max):
 
     return new_str
 
+# Alternative solution
+
+
+def max_repeated_chars(string, max):
+    if len(string) == 0 or max == 0:
+        return
+
+    new_str = ''
+    last_seen = None
+    last_seen_count = None
+
+    for char in string:
+        if char != last_seen:
+            last_seen = char
+            last_seen_count = 1
+        else:
+            last_seen_count += 1
+        if last_seen_count <= max:
+            new_str += char
+
+    return new_str
+
 
 print(max_repeated_chars('aaaabbbbccccdd', 1))
 print(max_repeated_chars('aaabbbcccccddddd', 3))
-print(max_repeated_chars('mmmaaaaarrtttin', 3))
+print(max_repeated_chars('mmmaaaaarrtttin', 1))
